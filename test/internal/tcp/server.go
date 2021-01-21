fe0940730161dd36f7e57ecbeacab5f56d511e6e
xyl <416319808@qq.com>
2021-01-21 17:26:45 +0800

    router

--- test/internal/tcp/server.go
+++ test/internal/tcp/server.go
@@ -1,8 +1,35 @@
 package main
 
-import "ego/entity"
+import (
+	"ego/entity"
+	"ego/iface"
+	"fmt"
+)
+
+type PingRouter struct {
+	entity.BaseRouter
+}
+
+func (r *PingRouter) Before(request iface.IRequest) {
+	if _, err := request.GetConnection().GetTCPConnection().Write([]byte("before...\n")); err != nil {
+		fmt.Println("callback before error")
+	}
+}
+
+func (r *PingRouter) Handle(request iface.IRequest) {
+	if _, err := request.GetConnection().GetTCPConnection().Write([]byte(fmt.Sprintf("%s\n", string(request.GetData())))); err != nil {
+		fmt.Println("callback handle error")
+	}
+}
+
+func (r *PingRouter) After(request iface.IRequest) {
+	if _, err := request.GetConnection().GetTCPConnection().Write([]byte("after...\n")); err != nil {
+		fmt.Println("callback after error")
+	}
+}
 
 func main() {
 	server := entity.NewServer("test")
+	server.AddRouter(&PingRouter{})
 	server.Run()
 }
